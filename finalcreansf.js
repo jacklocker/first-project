141b109bc5d9a9a4595f77cef50c72a93ba6d7a4
jacklocker <freedas09@gmail.com>
2019-12-10 19:04:50 +0530

    fixed UI

--- finalcreansf.js
+++ finalcreansf.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $(third);
+
     /* make main array*/
     if (localStorage.getItem('testObject') == null) {
         var main = [];
@@ -20,15 +22,27 @@ $(document).ready(function () {
         return age;
     };
     function createDetailbox(key, value) {
-        $("#demo").append('<div  id= "'
-            + '" myDivIdentifier="' + key + '"   style = " width: 250px; height: 210px; position: relative; margin-bottom: 20px;'
-            + ' margin-top: 20px; float :left; margin-left : 10px; background-color: tomato;">'
-            + '<button class = "toggel">Toggle</button><button class = "editbtnform">EDIT</button>'
-            + '<button class = "removebtn">DELET</button><div class = "side"><p>Fname:<br>Lname:<br>DOB:<br>Age:</p></div>'
-            + '<div class = "fnameBox">' + (value.value) + '</div><div class = "lnameBox">' + (value.value2) + '</div>'
-            + '<div class = "dobBox">' + (value.value3) + '</div><div class = "ageBox">' + (value.value5) + '</div></div>');
+        $("#demo").append('<div myDivIdentifier="' + key + '" class= " hoverEvent"  style = " width: 250px; height: 160px; position: relative; margin-bottom: 20px;'
+        + 'margin-top: 20px; float :left; margin-left : 12px; background-color : white; border-radius: 8px; z-index: 10;">'
+        + '<div myDivIdentifier="' + key + '" class="insideDivbtn">'
+        + ' <i  class="fa fa-trash removebtn"></i>'
+        + ' <i class="fa fa-edit editbtnform"></i>'
+        + ' <i class="fa fa-arrows-alt"></i>'
+        + '</div>'
+        + ' <div class="side">FName</div>'
+        + ' <div class="fnameBox">' + (value.value) + '</div>'
+        + ' <div class="side">LName</div>'
+        + ' <div class="lnameBox">' + (value.value2) + '</div>'
+        + '  <div class="side">DOB</div>'
+        + ' <div class="dobBox">' + (value.value3) + '</div>'
+        + ' <div class="side">Age</div>'
+        + ' <div class="ageBox">' + (value.value5) + '</div>'
+        + '</div>');
+
+        
     };
     function textfieldinFname(findindex) {
+        removeButonandopacity(findindex);
         $("[ myDivIdentifier =" + findindex + "]").find(".fnameBox").html('<input id = "newValuefname" type="text" placeholder="FName">');
         $("[ myDivIdentifier =" + findindex + "]").find(".fnameBox").find("#newValuefname").focus();
         $("[ myDivIdentifier =" + findindex + "]").find(".fnameBox").on('keydown', '#newValuefname', function (e) {
@@ -88,11 +102,18 @@ $(document).ready(function () {
         $("[ myDivIdentifier =" + l + "]").css({ "border": "5px solid red" });
         $("[ myDivIdentifier =" + (main.length - 1) + "]").css({ "border": "0px" });
     };
-    $("#first").click(function () {
-        $("#demo").empty();
-        var fname = $("#fname").val();
-        var lname = $("#lname").val();
-        var dob = $("#date").val();
+    function removeButonandopacity(findindex){
+        $("[ myDivIdentifier =" + findindex + "]").find(".insideDivbtn").stop().slideUp("slow");
+    };
+    function addButonandopacity(findindex){
+        $("[ myDivIdentifier =" + findindex + "]").find(".insideDivbtn").slideDown("slow");
+    };
+    function newElementadd(findindex){
+    
+        
+        var fname = $("[ myDivIdentifier =" + findindex + "]").find('#fnames').val();
+        var lname = $("[ myDivIdentifier =" + findindex + "]").find("#lname").val();
+        var dob = $("[ myDivIdentifier =" + findindex + "]").find("#date").val();
         var age = agecalculator(dob);
         var myObj = {};
         myObj.key = 'fname';
@@ -105,7 +126,9 @@ $(document).ready(function () {
         myObj.value4 = age;
         insertinMainarray(myObj);
         updateMainarray();
-    });
+        $("#third").trigger('click');
+    };
+    
     $(document).on("click", ".removebtn", function () {
         findindex = $(this).closest("div").attr("myDivIdentifier");
         main.splice(findindex, 1);
@@ -113,16 +136,45 @@ $(document).ready(function () {
         $('#' + findindex).remove();
         $("#third").trigger('click');
     });
+   
+    $(document).on("mouseenter", ".hoverEvent", function (){
+        findindex = $(this).closest("div").attr("myDivIdentifier");
+        addButonandopacity(findindex);
+        
+    });
+     
+    $(document).on("mouseleave", ".hoverEvent", function (){
+        findindex = $(this).closest("div").attr("myDivIdentifier");
+        removeButonandopacity(findindex);
+    });
+    
+function third(){
+    $("#demo").empty();
+    var normalMainarray = JSON.parse(localStorage.getItem('testObject'));
+    $.each(normalMainarray, function (key, value) {
+        createDetailbox(key, value);
+        if(key==main.length-1)
+        {
+            index= (main.length);
+            $("#demo").append('<div myDivIdentifier="' + index + '" style = " width: 250px; height: 180px; position: relative; margin-bottom: 20px;'
+            + 'margin-top: 20px; float :left; margin-left : 12px; background-color : white; border-radius: 8px; z-index: 10;">'
+            + ' <i id="formPopup" class="fa fa-plus-circle"></i></div>')
+        }
+    });
+}
     $("#third").click(function () {
-        $("#demo").empty();
-        var normalMainarray = JSON.parse(localStorage.getItem('testObject'));
-        $.each(normalMainarray, function (key, value) {
-            createDetailbox(key, value);
-        });
+        third();
     });
     $(document).on("click", ".editbtnform", function () {
         findindex = $(this).closest("div").attr("myDivIdentifier");
         textfieldinFname(findindex);
+    });
+    $(document).on("click", "#formPopup", function () {
+        findindex = $(this).closest("div").attr("myDivIdentifier");
+        $("[ myDivIdentifier =" + findindex + "]").html('<p></p><input  id="fnames" type="text" placeholder="First Name"><p><br></p><input  id="lname" type="text" placeholder="Last Name"><p><br></p><input id="date" type="date" placeholder="DOB"><p><br></p><button id="first" type="button" class="btn btn-success">Success</button>  ')
+        $("[ myDivIdentifier =" + findindex + "]").find("#first").click(function () {
+            newElementadd(findindex);
+        });
     });
     var l = localStorage.getItem('Indexs');
     $(document).on("keydown", "#third", function (e) {
@@ -139,4 +191,8 @@ $(document).ready(function () {
             textfieldinFname(l - 1);
         }
     });
+
+
+
+
 });
